17aec9c8cb1835e4bb6cade6ffe70aff8b692356
Rimon Hanna <rimon.hanna@live.com>
2022-06-10 10:37:41 +0200

    error reporting

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 from spotifyToYoutube import SpotifyToYoutube
     
 app = Flask(__name__)
@@ -13,37 +13,44 @@ def home():
 @app.route('/migrate', methods=['POST'])
 def migrate():
     args = request.json
-        
-    response_content_type = {'ContentType':'application/json'}
     
     if not args.get("spotify_playlists") or not args.get("ytmusic_playlists"):
-        return json.dumps({'success': False, 'message': 'Missing source or destination playlists'}), 500, response_content_type
+        return make_response(jsonify(success= False, message= 'Missing source or destination playlists'), 500)
 
-    source_playlists = json.loads(args.get("spotify_playlists"))
-    target_playlists = json.loads(args.get("ytmusic_playlists"))
+    try:
+        source_playlists = json.loads(args.get("spotify_playlists"))
+        target_playlists = json.loads(args.get("ytmusic_playlists"))
+    except Exception as e:
+        print("An exception occurred:", e)
+        return make_response(make_response(jsonify(success= False, message= str(e) + ', make sure that spotify_playlists and ytmusic_playlists are in the correct format ["playlist_1", "playlist_2"] including the square brackets, qoutes and comma separators'), 500))
     
-    if not args.get("ytmusic_headers"):
-        return json.dumps({'success': False, 'message': 'Missing YouTube Music Headers'}), 500, response_content_type
-    
-    spotify_to_youtube = SpotifyToYoutube()
-    ytmusic = spotify_to_youtube.login_to_google(args.get("ytmusic_headers"))
+    try:  
+        if not args.get("ytmusic_headers"):
+            return make_response(jsonify(success= False, message= 'Missing YouTube Music Headers'), 500)
+        
+        spotify_to_youtube = SpotifyToYoutube()
+        ytmusic = spotify_to_youtube.login_to_google(args.get("ytmusic_headers"))
 
-    if len(source_playlists) != len(target_playlists):
-        print("Please use the same number of Source and Target playlists")
-        return json.dumps({'success': False, 'message': 'Please use the same number of Source and Target playlists'}), 500, response_content_type
-    else:
-        for index, playlist_url in enumerate(source_playlists):
-            print(playlist_url)
-            print("Getting tracks...")
-            tracks = spotify_to_youtube.get_tracks(playlist_url, args.get("spotify_client_id"), args.get("spotify_client_secret"))
-            
-            target_playlist = target_playlists[index]
-            print(target_playlist)
-            target_playlist_id = ytmusic.create_playlist(target_playlist, target_playlist)
-
-            for track_index, track in enumerate(tracks):
-                print("Track", track_index + 1, "out of", len(tracks))
-                spotify_to_youtube.add_to_playlist(ytmusic, track, target_playlist_id)
+        if len(source_playlists) != len(target_playlists):
+            print("Please use the same number of Source and Target playlists")
+            return make_response(jsonify(success= False, message= 'Please use the same number of Source and Target playlists'), 500)
+        else:
+            for index, playlist_url in enumerate(source_playlists):
+                print(playlist_url)
+                print("Getting tracks...")
+                tracks = spotify_to_youtube.get_tracks(playlist_url, args.get("spotify_client_id"), args.get("spotify_client_secret"))
                 
-        print("Migration finished!")
-        return json.dumps({'success':True}), 200, response_content_type
+                target_playlist = target_playlists[index]
+                print(target_playlist)
+                target_playlist_id = ytmusic.create_playlist(target_playlist, target_playlist)
+
+                for track_index, track in enumerate(tracks):
+                    print("Track", track_index + 1, "out of", len(tracks))
+                    spotify_to_youtube.add_to_playlist(ytmusic, track, target_playlist_id)
+                    
+            print("Migration finished!")
+            return jsonify(success= True)
+    
+    except Exception as e:
+        print("An exception occurred:", e)
+        return make_response(make_response(jsonify(success= False, message= str(e)), 500))
